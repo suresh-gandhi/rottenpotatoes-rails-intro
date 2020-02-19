26181035f12683694ad1dbfda865b2ea546bbd1d
EC2 Default User <ec2-user@ip-172-31-27-101.us-east-2.compute.internal>
2020-02-19 01:30:30 +0000

    part 3 complete with code reorganization, bug fixes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,33 +5,55 @@ class MoviesController < ApplicationController
   end
 
   def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
+    id = params[:id] # Retreiving movie ID from the URI route
+    @movie = Movie.find(id) # Looking up the movie by unique ID
+    
     # will render app/views/movies/show.<extension> by default
   end
 
   def index
-    @all_ratings = Movie.ratings #Get all ratings
-    if(params[:ratings] == nil) 
-      @checked_ratings = @all_ratings
-    else
-      @checked_ratings = params[:ratings].keys
+    @all_ratings = Movie.ratings # Getting all ratings
+    
+    flag = false
+    
+    if(params[:order])
+      @order = params[:order]
+    elsif(session[:order])
+      @order = session[:order]
+      flag = true
     end
-    if(session[:ratings] != params[:ratings] && params[:ratings]!=nil) #Store ratings in session
-      session[:ratings] = params[:ratings]
-    elsif(session[:ratings]!=nil)
-      @checked_ratings = session[:ratings].keys
-    end
-    if (session[:ratings] != nil)
-      @movies = Movie.where(rating: session[:ratings].keys) #Filter based on ratings stored in session
+
+    if(params[:ratings])
+      @checked_ratings = params[:ratings]
+    elsif(session[:ratings])
+      @checked_ratings = session[:ratings]
+      flag = true
     else
+      @all_ratings.each do |rating|
+        (@checked_ratings ||= {}) [rating]=1
+      end
+    end
+    
+    session[:ratings] = @checked_ratings
+    session[:order] = @order
+
+    if flag
+      redirect_to movies_path(:order => @order, :ratings => @checked_ratings)
+    end
+    
+    if (session[:ratings] == nil)
       @movies = Movie.all
+    else
+      @movies = Movie.where(rating: @checked_ratings.keys) # Filtering based on ratings stored in session
     end
-    if(params[:order] == :title.to_s)
-      @movies = @movies.order(:title).all
-    elsif (params[:order] == :release_date.to_s)
+    
+    # Order based on the user input 
+    if(@order == :release_date.to_s)
       @movies = @movies.order(:release_date).all
+    elsif (@order == :title.to_s)
+      @movies = @movies.order(:title).all
     end
+    
   end
 
   def new
